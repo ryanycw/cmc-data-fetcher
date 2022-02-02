d5fd9d2e5ea974badc94188efe0d563b7904be12
ryanycw <ryan0213.tem04@nctu.edu.tw>
2022-02-02 23:47:27 +0800

    fetch id by symbol

---
+++ main.py
@@ -0,0 +1,45 @@
+import argparse
+from decouple import config
+from requests import Request, Session
+from requests.exceptions import ConnectionError, Timeout, TooManyRedirects
+import json
+
+parser = argparse.ArgumentParser(description='get symbol to search')
+parser.add_argument('-sym', '--symbol', type=str, help='symbol to get the ID.')
+args = parser.parse_args()
+
+urlMetadata = 'https://pro-api.coinmarketcap.com/v1/cryptocurrency/info'
+
+paramsMetadata = {
+  'symbol': f'{str(args.symbol)}',
+}
+
+headers = {
+  'Accepts': 'application/json',
+  'X-CMC_PRO_API_KEY': config('API_KEY'),
+}
+
+session = Session()
+session.headers.update(headers)
+
+try:
+  response = session.get(urlMetadata, params=paramsMetadata)
+  data = json.loads(response.text)
+  print(data['data'][f'{str(args.symbol)}']['id'])
+except (ConnectionError, Timeout, TooManyRedirects) as e:
+  print(e)
+
+"""
+urlOHLCVData = 'https://pro-api.coinmarketcap.com/v1/cryptocurrency/ohlcv/historical'
+
+paramsOHLCV = {
+
+}
+
+try:
+  response = session.get(urlMetadata, params=paramsMetadata)
+  data = json.loads(response.text)
+  print(data)
+except (ConnectionError, Timeout, TooManyRedirects) as e:
+  print(e)
+"""
